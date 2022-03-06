4ff752c88352fd57988d7268779dd1c8593c12d1
brinkqiang <brink.qiang@gmail.com>
2022-03-06 21:51:17 +0800

    add

--- src/dmbigint/dmbigint.cpp
+++ src/dmbigint/dmbigint.cpp
@@ -245,10 +245,51 @@ uint32_t CDMBigInt::GetCompact(bool fNegative) const
     return nCompact;
 }
 
+int islittleendian(void)
+{
+    int	littlendian = 1;
+    char* endptr = (char*)&littlendian;
+    return (int)*endptr;
+}
+
+inline uint16_t bswap_16(uint16_t x)
+{
+    if (islittleendian())
+    {
+        return x;
+    }
+    return (x >> 8) | (x << 8);
+}
+
+inline uint32_t bswap_32(uint32_t x)
+{
+    if (islittleendian())
+    {
+        return x;
+    }
+    return (((x & 0xff000000U) >> 24) | ((x & 0x00ff0000U) >> 8) |
+        ((x & 0x0000ff00U) << 8) | ((x & 0x000000ffU) << 24));
+}
+
+inline uint64_t bswap_64(uint64_t x)
+{
+    if (islittleendian())
+    {
+        return x;
+    }
+    return (((x & 0xff00000000000000ull) >> 56)
+        | ((x & 0x00ff000000000000ull) >> 40)
+        | ((x & 0x0000ff0000000000ull) >> 24)
+        | ((x & 0x000000ff00000000ull) >> 8)
+        | ((x & 0x00000000ff000000ull) << 8)
+        | ((x & 0x0000000000ff0000ull) << 24)
+        | ((x & 0x000000000000ff00ull) << 40)
+        | ((x & 0x00000000000000ffull) << 56));
+}
+
 void static inline WriteLE32(unsigned char* ptr, uint32_t x)
 {
-    //uint32_t v = htole32(x);
-    uint32_t v = x;
+    uint32_t v = bswap_32(x);
     memcpy(ptr, (char*)&v, 4);
 }
 
@@ -264,8 +305,7 @@ uint32_t static inline ReadLE32(const unsigned char* ptr)
 {
     uint32_t x;
     memcpy((char*)&x, ptr, 4);
-    return x;
-    //return le32toh(x);
+    return bswap_32(x);
 }
 
 CDMBigInt UintToArith256(const uint256& a)
