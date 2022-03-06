3f098a5774c82b6b73fd37d971aa71856834bcd2
brinkqiang <brink.qiang@gmail.com>
2022-03-06 22:09:32 +0800

    add

--- test/dmbiginttest/dmbiginttest.cpp
+++ test/dmbiginttest/dmbiginttest.cpp
@@ -4,7 +4,10 @@
 
 int main(int argc, char* argv[]) {
 
-    CDMBigInt bigint = 1;
+    CDMBigInt bigint("0000000000000000000000000000000000000000000000000000000000000001");
+
+    std::cout << bigint.ToString() << std::endl;
+
     bigint += 0x1233;
 
     std::cout << bigint.ToString() << std::endl;
